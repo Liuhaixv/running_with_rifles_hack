ff632692c28e4b06235927026e42c960442972bb
Liuhaixv <49237108+Liuhaixv@users.noreply.github.com>
2022-10-04 13:16:56 +0800

    release commit

---
+++ client.hpp
@@ -0,0 +1,61 @@
+#pragma once
+#include "memory.hpp"
+class Client {
+public :
+	Client(Memory memory) {
+		this->memory = memory;
+	}
+	int get_crosshair_status() {
+		/*
+		no target:0
+		yellow crosshair point(clear to shoot enemy):1
+		red crosshair point(unable to hit aim-point):2
+		not in game or dead:3 or 11
+
+		rwr_game.exe+004FB4D4 -> 8 24 2c 8 8 1494 8
+		*/
+		DWORD offsetToGameBaseAdress = 0x004FB4D4;
+		int crosshairStatusOffsets[] = { 0x8,0x24 ,0x2c ,0x8 ,0x8 ,0x1494 ,0x8 };
+
+		DWORD crosshairStatusAddress = NULL;
+
+		crosshairStatusAddress = memory.FindPointer(7,memory.gameBaseAddress + offsetToGameBaseAdress, crosshairStatusOffsets);
+
+		int crosshair_status = memory.read_mem<int>(crosshairStatusAddress);
+		return crosshair_status;
+	}
+
+	//use simulate mouse£¬may be replaced with writing memory method
+	void shoot() {
+		mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);//°´ÏÂ×ó¼ü
+		Sleep(20);
+		mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);//ËÉ¿ª×ó¼ü
+	}
+
+	void teleport_to(Point point) {
+		teleport_to(point.x, point.z, point.y);
+	}
+
+	void teleport_to(float x, float z, float y) {
+		TwoPoint to_point = { x,z,y ,x,z,y};
+
+		DWORD offsetToGameBaseAdress = 0x004FB4D4;
+		int playerPositonPoint_Offsets[] = { 0x2c, 0x20 ,0x24 ,0x20, 0x0, 0x34, 0xD18, 0x4, 0xB4 ,0x4};
+
+		DWORD playerPositionPoint_addr = memory.FindPointer(10, memory.gameBaseAddress + offsetToGameBaseAdress, playerPositonPoint_Offsets);
+
+		memory.write_mem(playerPositionPoint_addr, to_point);
+	}
+
+	Point get_crosshair_position() {
+		DWORD offsetToGameBaseAdress = 0x004FB4D4;
+		int crosshairPositionOffsets[] = { 0x8,0x24 ,0x2c ,0x8 ,0x8 ,0x1494 ,0x28 };
+
+		DWORD crosshairPosition_addr = memory.FindPointer(7, memory.gameBaseAddress + offsetToGameBaseAdress, crosshairPositionOffsets);
+
+		Point crosshairPosition = memory.read_mem<Point>(crosshairPosition_addr);
+		return crosshairPosition;
+	}
+private:
+	Memory memory;
+};
---
+++ memory.hpp
@@ -0,0 +1,120 @@
+#pragma once
+#include <Windows.h>
+#include<TlHelp32.h>
+#include <iostream>
+#include <tchar.h> // _tcscmp
+#include <vector>
+
+
+class Memory {
+public:
+	DWORD pID = NULL;
+	HANDLE processHandle = NULL;
+	DWORD gameBaseAddress = NULL;
+
+	Memory() {
+
+		pID = get_porcId_by_name("rwr_game.exe");
+		if (pID == NULL) {
+			std::cout << "Launch the game before running hack!" << std::endl;
+			return;
+		}
+		std::cout << pID << std::endl;
+
+		processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pID);
+		if (processHandle == INVALID_HANDLE_VALUE || processHandle == NULL) { // error handling
+			std::cout << "Failed to open process" << std::endl;
+			return;
+		}
+
+		char gameName[] = "rwr_game.exe";
+		gameBaseAddress = GetModuleBaseAddress(_T(gameName), pID);
+	}
+
+	template <typename var>
+	bool write_mem(DWORD address, var value) {
+		return WriteProcessMemory(processHandle, (LPVOID)address, &value, sizeof(var), NULL);
+	}
+
+	template <typename var>
+	var read_mem(DWORD address) {
+		var value;
+		ReadProcessMemory(processHandle, (LPCVOID)address, &value, sizeof(var), NULL);
+		return value;
+	}
+
+	DWORD FindPointer(int offset, DWORD baseaddr, int offsets[])
+	{
+		DWORD Address = baseaddr;
+		int total = offset;
+		for (int i = 0; i < total; i++) //Loop trough the offsets
+		{
+			ReadProcessMemory(processHandle, (LPCVOID)Address, &Address, sizeof(DWORD), NULL);
+			Address += offsets[i];
+		}
+		return Address;
+	}
+private:
+
+	DWORD GetModuleBaseAddress(TCHAR* lpszModuleName, DWORD pID) {
+		DWORD dwModuleBaseAddress = 0;
+		HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, pID); // make snapshot of all modules within process
+		MODULEENTRY32 ModuleEntry32 = { 0 };
+		ModuleEntry32.dwSize = sizeof(MODULEENTRY32);
+
+		if (Module32First(hSnapshot, &ModuleEntry32)) //store first Module in ModuleEntry32
+		{
+			do {
+				if (_tcscmp(ModuleEntry32.szModule, lpszModuleName) == 0) // if Found Module matches Module we look for -> done!
+				{
+					dwModuleBaseAddress = (DWORD)ModuleEntry32.modBaseAddr;
+					break;
+				}
+			} while (Module32Next(hSnapshot, &ModuleEntry32)); // go through Module entries in Snapshot and store in ModuleEntry32
+
+
+		}
+		CloseHandle(hSnapshot);
+		return dwModuleBaseAddress;
+	}
+
+	static DWORD get_porcId_by_name(const std::string_view targetProcess) {
+		DWORD procId = 0;
+		HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+		if (hSnap != INVALID_HANDLE_VALUE)
+		{
+			PROCESSENTRY32 procEntry;
+			procEntry.dwSize = sizeof(procEntry);
+			if (Process32First(hSnap, &procEntry))
+			{
+				do
+				{
+					if (!targetProcess.compare(procEntry.szExeFile))
+					{
+						procId = procEntry.th32ProcessID;
+						//std::cout << "found pID:" << procId << std::endl;
+						//break;
+					}
+				} while (Process32Next(hSnap, &procEntry));
+			}
+		}
+		CloseHandle(hSnap);
+
+		return procId;
+	}
+};
+
+struct Point {
+	float x;
+	float z;
+	float y;
+};
+
+struct TwoPoint {
+	float x1;
+	float z1;
+	float y1;
+	float x2;
+	float z2;
+	float y2;
+};
